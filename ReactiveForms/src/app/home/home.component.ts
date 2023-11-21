b8bcb40016f44d3ac28c94b8e916e1296854bbf5
NancyBatham <nancy751batham@gmail.com>
2023-11-21 17:57:45 +0530

    dynamic forms

--- ReactiveForms/src/app/home/home.component.ts
+++ ReactiveForms/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,21 +7,61 @@ import { FormGroup,FormControl } from '@angular/forms';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+
+  constructor(private formBuilder:FormBuilder) {}
   name = new FormControl('');
+  myForm : FormGroup|any;
   UpdateName(){
     this.name.setValue('Nancy');
   }
 profileForm= new FormGroup({
-  fname: new FormControl(''),
-  lname: new FormControl('')
+  fname: new FormControl('', Validators.required),
+  lname: new FormControl('',  Validators.required)
 });
+
 
 onSubmit(){
   console.log(this.profileForm.controls);
   console.log(this.profileForm.value);
   console.log(this.profileForm.value.fname);
+}  
+
+onSubmitD(){
+  console.log(this.myForm.controls);
+  console.log(this.myForm.value);
+}  
+
+
+ngOnInit() {
+  
+  this.myForm = this.formBuilder.group({
+    fields: this.formBuilder.array([])
+    
+  });
+  const field = this.formBuilder.group({
+    fieldName: [''],
+    fieldDescription: [''],
+    // Add other fields as needed
+  });
+
+  this.fieldForms.push(field);
 }
 
-  ngOnInit(): void {}
+get fieldForms() {
+  return this.myForm.get('fields') as FormArray;
+}
+
+addField() {
+  const field = this.formBuilder.group({
+    fieldName: [''],
+    fieldDescription: [''],
+    // Add other fields as needed
+  });
+
+  this.fieldForms.push(field);
+}
+
+removeField(index: number) {
+  this.fieldForms.removeAt(index);
+}
 }
